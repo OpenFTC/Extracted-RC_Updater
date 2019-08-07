e660c96950d99a3ed05e470e070204fc0868fa62
FROGbots-4634 <teamfrogbots@gmail.com>
2019-08-07 17:06:17 -0400

    Minor change to checklist

--- src/org/openftc/Updater.java
+++ src/org/openftc/Updater.java
@@ -687,7 +687,7 @@ public class Updater
         System.out.println("=======================================================");
         System.out.println();
 
-        System.out.println("1. Remove the version code and name for each library from its manifest");
+        System.out.println("1. Remove the version code, version name, and 'uses-sdk' tag for each library from its manifest");
         System.out.println("2. Update the version code and name for each library in its build.gradle file");
         System.out.println("3. Use AS to reformat all text files to LF line endings (select root project folder, File -> Line Endings -> LF)");
         System.out.println();
